3903abb68d54c58ca6173674416c341560677353
Matthew-McCulley <matthewmcculley9@gmail.com>
2023-10-15 15:44:04 -0400

    Updated folders hopefully

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use('/api/users', userRoutes)
 if(process.env.NODE_ENV === 'production'){
     const __dirname = path.resolve()
     app.use(express.static(path.join(__dirname, 'frontend/dist')))
-    app.get('*', (req, res) => res.sendFile(oath.resolve(__dirname, 'frontend', 'dist',
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist',
     'index.html')))
 }else{
     app.get('/', (req,res) => res.send('Server is ready'))
